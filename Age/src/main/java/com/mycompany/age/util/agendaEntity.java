f82e6d4ffff28c2619a1a329c2f68ae711463ebe
Edward Raul Llancay <estudiante@UCUXA60635.continental.edu.pe>
2023-07-05 17:07:45 -0500

    agent

--- Age/src/main/java/com/mycompany/age/util/agendaEntity.java
+++ Age/src/main/java/com/mycompany/age/util/agendaEntity.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.age.util;
 
-/**
- *
- * @author estudiante
- */
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class agendaEntity {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("agenda");
     
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
 }
