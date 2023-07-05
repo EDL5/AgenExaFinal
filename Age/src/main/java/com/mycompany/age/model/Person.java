8b3b6403d7581409a4f10adfa31d5c9dbf59fe69
Edward Raul Llancay <estudiante@UCUXA60635.continental.edu.pe>
2023-07-05 17:02:33 -0500

    agendi

--- Age/src/main/java/com/mycompany/age/model/Person.java
+++ Age/src/main/java/com/mycompany/age/model/Person.java
@@ -38,6 +38,14 @@ public class Person {
 		this.city = new SimpleStringProperty("some city");
 		this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
 	}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
 	
 	public String getFirstName() {
 		return firstName.get();
